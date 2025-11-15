85ff6d2ede8f6942622606d144ef1fec87a8743b
highrohan69-lab <high.rohan69@gmail.com>
2025-11-15 13:43:36 +0600

    first commit

--- src/components/BannerSection.vue
+++ src/components/BannerSection.vue
@@ -36,7 +36,7 @@ import axios from "axios";
 const API_BASE =
   window.location.hostname === "localhost"
     ? "http://localhost:5000/api"
-    : "https://your-backend.com/api";
+    : "https://avadotechbackend.onrender.com";
 
 const banners = ref([]);
 const current = ref(0);
